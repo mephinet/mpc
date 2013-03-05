81bd18f55ac9f4f3200512ee704f3ff1ff58ea8f
mephinet <mephinet@gmx.net>
2013-03-05 21:48:47 +0100

    about box formatted

--- app/source/About.js
+++ app/source/About.js
@@ -4,17 +4,18 @@
 enyo.kind({
     name: "MPC.About",
     kind: enyo.Popup,
+    className: "enyo-popup aboutPopup",
 
     components: [
         {kind: enyo.HFlexBox, components: [
-            {kind: enyo.Image, className: "aboutImage", src: "images/icon_64x64.png"},
+            {kind: enyo.Image, className: "icon", src: "images/icon_64x64.png"},
             {kind: enyo.VFlexBox, components: [
-                {name: "title", className: "aboutTitle"},
-                {name: "copyright", className: "aboutCopyright"}
+                {name: "title", className: "title"},
+                {name: "copyright", className: "copyright"}
             ]}
         ]},
-        {name: "licenseInfo", className: "licenseInfo", content: 'This app uses <a href="http://mpd.wikia.com/wiki/ClientLib:libmpdclient">libmpdclient</a>, which is covered under a revised BSD license. The app icon was designed by <a href="http://mugenb16.deviantart.com/">MugenB16</a> and is covered by a CC license.', allowHtml: true},
-        {name: "info", allowHtml: true},
+        {className: "licenseInfo", content: 'This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.<br /> This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.<br /> You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.<br />This app is licensed  uses <a href="http://mpd.wikia.com/wiki/ClientLib:libmpdclient">libmpdclient</a>, which is covered under a revised BSD license.<br />The icon was designed by <a href="http://mugenb16.deviantart.com/">MugenB16</a> and is covered by a CC license.', allowHtml: true},
+        {name: "info", className: "info", allowHtml: true},
         {kind: enyo.Button, caption: $L("close"), onclick: "close"}
     ],
 
